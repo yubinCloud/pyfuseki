26c66ea9a681d97e8553fd432fb352f7d0a14ce7
yubinCloud <yubin_SkyWalker@yeah.net>
2021-02-18 20:30:38 +0800

    更新版本号为 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyfuseki",
-    version="1.0.1",
+    version="1.0.2",
     author="Bin Yu",
     author_email="yubin_SkyWalker@yeah.net",
     description="An easy way to mix together OWL and Jena Fuseki.",
