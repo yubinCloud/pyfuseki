719d2027031e7c2697bcf06c406b3dfecb1366d5
yubinCloud <yubin_SkyWalker@yeah.net>
2021-02-18 17:14:37 +0800

    为 BaseRdfPrefixEnum 添加 uid 方法，并在val方法内自动将value转换为 str 类型

--- pyfuseki/ontology_mapper/BaseRdfPrefixEnum.py
+++ pyfuseki/ontology_mapper/BaseRdfPrefixEnum.py
@@ -6,13 +6,14 @@
 """
 from enum import Enum
 import rdflib
-from typing import Union
+from typing import Union, Any
+import uuid
 
 class BaseRdfPrefixEnum(Enum):
     """
     All RDFPrefixEnum class should inherit from it.
     """
-    def val(self, value: str = None) -> Union[rdflib.BNode, rdflib.URIRef]:
+    def val(self, value: Any = None) -> Union[rdflib.BNode, rdflib.URIRef]:
         """
         以该namespace和传入的value生成一个URIRef或BNode或Literal
         :param value: 所生成的Identifier的值
@@ -21,4 +22,11 @@ class BaseRdfPrefixEnum(Enum):
         if value is None:
             return rdflib.BNode()
         else:
-            return self.value[value]
+            return self.value[str(value)]
+
+    def uid(self) -> rdflib.URIRef:
+        """
+        以 uuid 生成一个唯一 id 来作为 value 包装成 URIRef
+        :return:
+        """
+        return self.value[str(uuid.uuid1())]
